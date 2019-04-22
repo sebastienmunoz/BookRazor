2c4887b3b96fcf89f21d167f694fb6f1fd87278b
sebastienmunoz <35344933+sebastienmunoz@users.noreply.github.com>
2019-04-22 15:21:09 +0200

    pageCreated

---
+++ BookRazor/Models/Book.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BookRazor.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Isbn { get; set; }
+        public string PublisherName { get; set; }
+        public string AuthorName { get; set; }
+        public string CategoryName { get; set; }
+
+        //Get all books
+        public List<Book> GetBooks(string connectionString)
+        {
+            List<Book> bookList = new List<Book>();
+            SqlConnection con = new SqlConnection(connectionString);
+            string selectSQL = "select BookId, Title, Isbn, PublisherName, AuthorName, CategoryName from GetBookData";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(selectSQL, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr != null)
+            {
+                while (dr.Read())
+                {
+                    Book book = new Book();
+                    book.BookId = Convert.ToInt32(dr["BookId"]);
+                    book.Title = dr["Title"].ToString();
+                    book.Isbn = dr["Isbn"].ToString();
+                    book.PublisherName = dr["PublisherName"].ToString();
+                    book.AuthorName = dr["AuthorName"].ToString();
+                    book.CategoryName = dr["CategoryName"].ToString();
+
+                    bookList.Add(book);
+                }
+            }
+            con.Close();
+            return bookList;
+        }
+
+        //Create a new book
+        public void CreateBook(string connectionString, Book book)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("CreateBook", con);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add(new SqlParameter("@Title", book.Title));
+                    cmd.Parameters.Add(new SqlParameter("@Isbn", book.Isbn));
+                    cmd.Parameters.Add(new SqlParameter("@PublisherName", book.PublisherName));
+                    cmd.Parameters.Add(new SqlParameter("@AuthorName", book.AuthorName));
+                    cmd.Parameters.Add(new SqlParameter("@CategoryName", book.CategoryName));
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Book getBookData(string connectionString, int bookId)
+        {
+            Book book = new Book();
+            SqlConnection con = new SqlConnection(connectionString);
+            string selectSQL = "select BookId, Title, Isbn, PublisherName, AuthorName, CategoryName from GetBookData where BookId =" + bookId;
+            con.Open();
+            SqlCommand cmd = new SqlCommand(selectSQL, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr != null)
+            {
+                while (dr.Read())
+                {
+                    book.BookId = Convert.ToInt32(dr["BookId"]);
+                    book.Title = dr["Title"].ToString();
+                    book.Isbn = dr["Isbn"].ToString();
+                    book.PublisherName = dr["PublisherName"].ToString();
+                    book.AuthorName = dr["AuthorName"].ToString();
+                    book.CategoryName = dr["CategoryName"].ToString();
+                }
+            }
+            return book;
+        }
+
+        public void EditBook(string connectionString)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("UpdateBook", con);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@BookId", this.BookId));
+                    cmd.Parameters.Add(new SqlParameter("@Title", this.Title));
+                    cmd.Parameters.Add(new SqlParameter("@Isbn", this.Isbn));
+                    cmd.Parameters.Add(new SqlParameter("@PublisherName", this.PublisherName));
+                    cmd.Parameters.Add(new SqlParameter("@AuthorName", this.AuthorName));
+                    cmd.Parameters.Add(new SqlParameter("@CategoryName", this.CategoryName));
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public void DeleteBook(string connectionString, int bookId)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("DeleteBook", con);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@BookId", bookId));
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
