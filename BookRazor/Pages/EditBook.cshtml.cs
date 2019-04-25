94dd041358afb88df9218ad9d7b955271a90e77b
sebastienmunoz <35344933+sebastienmunoz@users.noreply.github.com>
2019-04-25 19:37:28 +0200

    now

--- BookRazor/Pages/EditBook.cshtml.cs
+++ BookRazor/Pages/EditBook.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookRazor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,36 @@ namespace BookRazor.Pages
 {
     public class EditBookModel : PageModel
     {
-        public void OnGet()
-        {
 
+        public string connectionStr = new AppSettings().connectionStr;
+
+        [BindProperty]
+        public Book book { get; set; }
+
+        //public ActionResult OnGet(int? bookId)
+        //{
+        //    if (bookId == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    book = book.getBookData(connectionStr, bookId);
+
+        //    if (book == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Page();
+        //}
+
+        public ActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            book.EditBook(connectionStr);
+
+            return RedirectToPage("./Books");
         }
     }
 }
